283543d64b7ae075f2f8d5e552b1ad1c7f22f55b
slowli <slowli@users.noreply.github.com>
2024-10-13 12:27:22 +0000

    Deploying to gh-pages from @ slowli/test-casing@8012e6eb96e5d253527b8624b730204e212901b7 🚀

---
+++ src-files.js
@@ -0,0 +1,5 @@
+var srcIndex = new Map(JSON.parse('[\
+["test_casing",["",[],["decorators.rs","lib.rs","test_casing.rs"]]],\
+["test_casing_macro",["",[["test_casing",[],["mod.rs"]]],["decorate.rs","lib.rs"]]]\
+]'));
+createSrcSidebar();
